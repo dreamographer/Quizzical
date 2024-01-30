a34ec47445a0b33211c4e51053984043c18ef146
Ashwin <ashwinkv.akv@gmail.com>
2024-01-30 12:22:50 +0530

    updated fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
       <body className="w-full  text-white font-[poppins] font-medium ">{children}</body>
     </html>
   )
